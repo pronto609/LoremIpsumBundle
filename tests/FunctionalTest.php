8f06df0982b14407540657053ba750dd32863c6e
Jurij Leshchuk <ddevdev609@gmail.com>
2023-11-02 10:13:08 +0200

    Service Integration Test

---
+++ tests/FunctionalTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace KnpU\LoremIpsumBundle\Tests;
+
+use KnpU\LoremIpsumBundle\KnpUIpsum;
+use KnpU\LoremIpsumBundle\KnpULoremIpsumBundle;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Symfony\Component\HttpKernel\Kernel;
+
+class FunctionalTest extends TestCase
+{
+    public function testServiceWiring()
+    {
+        $kernel = new KnpULoremIpsumTestingKernel('test', true);
+        $kernel->boot();
+        $container = $kernel->getContainer();
+
+        $ipsum = $container->get('knpu-lorem-ipsum.knpu_ipsum');
+        $this->assertInstanceOf(KnpUIpsum::class, $ipsum);
+        $this->assertIsString('string', $ipsum->getParagraphs());
+    }
+}
+
+class KnpULoremIpsumTestingKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        return [
+            new KnpULoremIpsumBundle()
+        ];
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        // TODO: Implement registerContainerConfiguration() method.
+    }
+
+}
