107129bfd15fe2324043c0689143c405c1ae96b7
Jurij Leshchuk <ddevdev609@gmail.com>
2023-11-02 19:38:54 +0200

    Controller Functional Test

---
+++ tests/Controller/IpsumApiControllerTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace KnpU\LoremIpsumBundle\Test\Controller;
+
+use KnpU\LoremIpsumBundle\KnpULoremIpsumBundle;
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Routing\RouteCollectionBuilder;
+
+class IpsumApiControllerTest extends TestCase
+{
+    public function testIndex()
+    {
+        $kernel = new KnpULoremIpsumControllerKernel();
+        $client = new Client($kernel);
+        $client->request('GET', '/api/');
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+    }
+}
+
+class KnpULoremIpsumControllerKernel extends Kernel
+{
+    use MicroKernelTrait;
+    public function __construct()
+    {
+        parent::__construct('test', true);
+    }
+
+    public function registerBundles()
+    {
+        return [
+            new KnpULoremIpsumBundle(),
+            new FrameworkBundle()
+        ];
+    }
+
+    public function getCacheDir()
+    {
+        return __DIR__ . '/cache/' . spl_object_hash($this);
+    }
+
+
+    protected function configureRoutes(RouteCollectionBuilder $routes)
+    {
+        $routes->import(__DIR__.'/../../src/Resources/config/routs.xml', '/api');
+    }
+
+    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader)
+    {
+        $container->loadFromExtension('framework', [
+            'secret' => 'FOO'
+        ]);
+    }
+}
